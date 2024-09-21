51f35f1d64e2837914ebc385338affea36cbb9b8
Lukasz Szpilowski <91949571+LSzpilowski@users.noreply.github.com>
2024-09-21 21:42:21 +0100

    Add feat/results (#4)
    * add table to feat/results
    
    * add feat/results and display projects
    
    * display feat/statistics to fix styling

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+"use client";
+import { useState } from "react";
+import { Card } from "@/components/ui/card";
+import ResultsTable from "./resultsTable";
+import { FaListUl } from "react-icons/fa";
+import { IoBarChartSharp } from "react-icons/io5";
+import StatisticsTable from "./statisticsTable";
+
+const areas = ["South-East", "South-West", "North-East", "North-West"];
+interface ISwitchButtons {
+  title: string;
+  icon: JSX.Element;
+}
+const switchButtons: ISwitchButtons[] = [
+  {
+    title: "Results",
+    icon: <FaListUl />,
+  },
+  {
+    title: "Statistics",
+    icon: <IoBarChartSharp />,
+  },
+];
 
 function Results() {
-  return <div>Results</div>;
+  const [activeTab, setActiveTab] = useState("Results");
+  return (
+    <div className="min-h-screen w-full flex flex-col  items-center gap-5 bg-[#E4E4E4] pt-28 pb-10 ">
+      <Card className="flex flex-col gap-10 w-2/3 p-10 ">
+        <div className="w-full flex flex-row">
+          {switchButtons.map((item, index) => (
+            <div
+              key={index}
+              className={`w-1/2 flex flex-row  justify-center items-center gap-2 ${
+                activeTab === item.title
+                  ? "bg-blue-500 text-white"
+                  : "bg-[#cfcece] opacity-80 hover:opacity-100"
+              }  text-2xl  py-3 border-0 ${
+                item.title === "Results" ? "rounded-l-md" : "rounded-r-md"
+              } hover:cursor-pointer`}
+              onClick={() => setActiveTab(item.title)}
+            >
+              {item.icon} {item.title}
+            </div>
+          ))}
+        </div>
+        {activeTab === "Results" && <ResultsTable areas={areas} />}
+        {activeTab === "Statistics" && <StatisticsTable />}
+      </Card>
+    </div>
+  );
 }
 
 export default Results;
