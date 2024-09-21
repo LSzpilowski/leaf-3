b50e6c64b85e1e195d77c9f602869e6821fea544
LSzpilowski <lszpilowski@gmail.com>
2024-09-21 20:05:09 +0100

    add feat/results and display projects

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,55 +1,51 @@
-import { Item } from "@radix-ui/react-dropdown-menu";
-import { mockProjects } from "../mockData";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+"use client";
+import { useState } from "react";
+import { Card } from "@/components/ui/card";
+import ResultsTable from "./resultsTable";
+import { FaListUl } from "react-icons/fa";
+import { IoBarChartSharp } from "react-icons/io5";
 
 const areas = ["South-East", "South-West", "North-East", "North-West"];
+interface ISwitchButtons {
+  title: string;
+  icon: JSX.Element;
+}
+const switchButtons: ISwitchButtons[] = [
+  {
+    title: "Results",
+    icon: <FaListUl />,
+  },
+  {
+    title: "Statistics",
+    icon: <IoBarChartSharp />,
+  },
+];
 
 function Results() {
+  const [activeTab, setActiveTab] = useState("Results");
   return (
-    <div className="min-h-screen w-full flex flex-col justify-center items-center gap-5 bg-[#E4E4E4] mt-40 ">
-      <div className="flex flex-col gap-10 w-2/3">
-        {areas.map((area, index) => {
-          return (
-            <div key={index}>
-              <p className="w-full text-xl font-bold text-center pt-2">
-                {area}
-              </p>
-              <Table>
-                <TableHeader className="w-full">
-                  <TableRow>
-                    <TableHead className="w-[100px]">Place</TableHead>
-                    <TableHead>Title</TableHead>
-                    <TableHead>Cost</TableHead>
-                    <TableHead className="text-right">
-                      Amount of Points
-                    </TableHead>
-                    <TableHead className="text-right">Podgląd</TableHead>
-                  </TableRow>
-                </TableHeader>
-                {mockProjects
-                  .filter((project) => project.district === area)
-                  .map((item, index) => (
-                    <TableRow key={index}>
-                      <TableCell className="font-medium">{index + 1}</TableCell>
-                      <TableCell>{item.title}</TableCell>
-                      <TableCell>{item.estimatedCost}</TableCell>
-                      <TableCell className="text-right">points</TableCell>
-                      <TableCell className="text-right">$250.00</TableCell>
-                    </TableRow>
-                  ))}
-              </Table>
+    <div className="min-h-screen w-full flex flex-col justify-center items-center gap-5 bg-[#E4E4E4] pt-28 pb-10 ">
+      <Card className="flex flex-col gap-10 w-2/3 p-10 ">
+        <div className="w-full flex flex-row">
+          {switchButtons.map((item, index) => (
+            <div
+              key={index}
+              className={`w-1/2 text-center ${
+                activeTab === item.title
+                  ? "bg-blue-500 text-white"
+                  : "bg-[#E4E4E4] hover:opacity-80"
+              }  text-2xl  py-3 border-0 ${
+                item.title === "Results" ? "rounded-l-md" : "rounded-r-md"
+              } hover:cursor-pointer`}
+              onClick={() => setActiveTab(item.title)}
+            >
+              {item.title}
             </div>
-          );
-        })}
-      </div>
+          ))}
+        </div>
+        {activeTab === "Results" && <ResultsTable areas={areas} />}
+        {activeTab === "Statistics" && <div>Statystki</div>}
+      </Card>
     </div>
   );
 }
---
+++ src/app/results/resultsTable.tsx
@@ -0,0 +1,78 @@
+import { mockProjects } from "../mockData";
+import {
+  Table,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Card } from "@/components/ui/card";
+import { IoIosArrowDroprightCircle } from "react-icons/io";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import DialogProject from "../projects/dialogCard/dialogProject";
+import DialogMap from "../projects/dialogCard/dialogMap";
+
+interface IResultsTable {
+  areas: string[];
+}
+
+function ResultsTable({ areas }: IResultsTable) {
+  return (
+    <div className="flex flex-col gap-10 w-full  ">
+      {areas.map((area, index) => {
+        return (
+          <div key={index}>
+            <p className="w-full text-xl font-bold text-center py-5 bg-[#E4E4E4]">
+              Area no. {index + 1} - {area}
+            </p>
+            <Table>
+              <TableHeader className="w-full">
+                <TableRow className="hover:bg-white">
+                  <TableHead className="w-[100px]">Place</TableHead>
+                  <TableHead>Title</TableHead>
+                  <TableHead className="text-left">Cost</TableHead>
+                  <TableHead className="text-center">Number of Votes</TableHead>
+                  <TableHead className="text-right">Preview</TableHead>
+                </TableRow>
+              </TableHeader>
+              {mockProjects
+                .filter((project) => project.district === area)
+                .map((item, index) => (
+                  <Dialog key={index}>
+                    <TableRow
+                      className={`font-medium ${
+                        index === 0 ? "bg-blue-500 hover:bg-[blue-500]" : null
+                      }`}
+                    >
+                      <TableCell className="font-medium">{index + 1}</TableCell>
+                      <TableCell>
+                        <DialogTrigger>{item.title}</DialogTrigger>
+                      </TableCell>
+                      <TableCell className="text-left">
+                        {item.estimatedCost}
+                      </TableCell>
+                      <TableCell className="text-center">Votes</TableCell>
+                      <TableCell className="text-right">
+                        <DialogTrigger>
+                          <div className="flex flex-row gap-2 items-center justify-end m-0 p-0">
+                            <p>View</p>
+                            <IoIosArrowDroprightCircle />
+                          </div>
+                        </DialogTrigger>
+                      </TableCell>
+                    </TableRow>
+                    <DialogContent className="flex flex-row w-full h-screen gap-0  px-48 bg-transparent  border-none rounded-none ">
+                      <DialogProject project={item} index={index} />
+                      <DialogMap project={item} index={index} />
+                    </DialogContent>
+                  </Dialog>
+                ))}
+            </Table>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default ResultsTable;
