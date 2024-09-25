5b2ddc1cb6b99d0ff15c8a85122ac56b62968ba1
Lukasz Szpilowski <91949571+LSzpilowski@users.noreply.github.com>
2024-09-25 17:27:47 +0100

    finish feat/budget (#7)

--- src/app/about_the_budget/price_list_-_estimated_costs/page.tsx
+++ src/app/about_the_budget/price_list_-_estimated_costs/page.tsx
@@ -1,4 +1,268 @@
+import { Card } from "@/components/ui/card";
 import Image from "next/image";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+interface IPriceListContentDescSubdescs {
+  subdesc?: string;
+}
+interface IPriceListContentDesc {
+  title?: string;
+  subdescs?: IPriceListContentDescSubdescs[];
+}
+interface IPriceListContentDescs {
+  desc?: IPriceListContentDesc[];
+}
+
+interface IPriceListContent {
+  img?: string;
+  subtitle?: string;
+  descs?: IPriceListContentDescs[];
+  price?: string;
+}
+
+interface IPriceListContents {
+  content?: IPriceListContent[];
+}
+interface IPriceList {
+  title: string;
+  contents: IPriceListContents[];
+}
+
+const priceLists: IPriceList[] = [
+  {
+    title: "Roads, sidewalks, bike paths",
+    contents: [
+      {
+        content: [
+          {
+            img: "/images/bike_stand.jpg",
+            subtitle:
+              "Purchase and installation of a four-station bicycle rack",
+            price: "£108 – £216 / piece",
+          },
+        ],
+      },
+      {
+        content: [
+          {
+            img: "/images/road_reconstruction.jpeg",
+            subtitle: "Road reconstruction",
+            descs: [
+              {
+                desc: [
+                  {
+                    title:
+                      "Asphalt road (minimum width of residential road 5 meters): £117 / 1 m²",
+                  },
+                ],
+              },
+              {
+                desc: [
+                  {
+                    title: "Concrete blocks: £99 / 1 m²",
+                  },
+                ],
+              },
+              {
+                desc: [
+                  {
+                    title: "Dirt road reinforcement:",
+                    subdescs: [
+                      {
+                        subdesc: "Using asphalt milling waste – £9 / 1 m²",
+                      },
+                      {
+                        subdesc: "Crushed stone 0-31.5 mm – £14.40 / 1 m²",
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Small architecture / Playgrounds / Recreational areas",
+    contents: [
+      {
+        content: [
+          {
+            img: "/images/bench.jpg",
+            subtitle: "Purchase and installation of benches",
+            descs: [
+              {
+                desc: [
+                  {
+                    title: "Park bench: £180 – £540",
+                  },
+                ],
+              },
+              {
+                desc: [
+                  {
+                    title: "Youth bench without backrest: £270",
+                  },
+                ],
+              },
+              {
+                desc: [
+                  {
+                    title: "Hammocks: £1,080 – £2,160",
+                  },
+                ],
+              },
+              {
+                desc: [
+                  {
+                    title: "City deckchairs: £810 – £2,160",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        content: [
+          {
+            img: "/images/bin.jpg",
+            subtitle: "Purchase and installation of trash bins",
+            price: "£90 – £270 / piece",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Green areas and planting",
+    contents: [
+      {
+        content: [
+          {
+            img: "/images/tree.jpg",
+            subtitle: "Purchase and planting of trees",
+            descs: [
+              {
+                desc: [
+                  {
+                    title: "Tree planting services: £111.60 – £270 / tree",
+                  },
+                  {
+                    title: "Purchase of trees: £117 / tree",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        content: [
+          {
+            img: "/images/shrubs.jpg",
+            subtitle: "Planting ornamental shrubs",
+            price: "£7.20 / shrub",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Sports and recreation",
+    contents: [
+      {
+        content: [
+          {
+            img: "/images/goal.jpg",
+            subtitle: "Sport equipment",
+            descs: [
+              {
+                desc: [
+                  {
+                    title: "Football goals (handball): £1,992.60 / piece",
+                  },
+                  {
+                    title: "Basketball hoop: £2,214 / piece",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        content: [
+          {
+            img: "/images/field.jpg",
+            subtitle: "Construction of synthetic sports field",
+            price: "£75.60 – £138.60 / m²",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Lighting",
+    contents: [
+      {
+        content: [
+          {
+            img: "/images/lamp.jpg",
+            subtitle: "Installation of street lamps",
+            descs: [
+              {
+                desc: [
+                  {
+                    title: "Single-arm street lamp: £1,890 / piece",
+                  },
+                  {
+                    title: "Stylized street lamp: £3,060 / piece",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Drainage",
+    contents: [
+      {
+        content: [
+          {
+            img: "/images/drainage.jpg",
+            subtitle: "Stormwater drainage construction",
+            price: "£630 / meter",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Project documentation",
+    contents: [
+      {
+        content: [
+          {
+            img: "/images/documentation.jpg",
+            subtitle: "Preparation of project documentation",
+            price: "£11,700 – £27,000",
+          },
+        ],
+      },
+    ],
+  },
+];
 
 function PriceListEstimatedCost() {
   return (
@@ -16,7 +280,76 @@ function PriceListEstimatedCost() {
           <p className="text-4xl font-bold text-white">BUDGET STEP BY STEP</p>
         </div>
       </div>
-      <div className="flex flex-row justify-center w-3/4 min-h-[65vh] "></div>
+      <div className="flex flex-row justify-center w-3/4 min-h-[65vh] ">
+        <Card className="w-2/3 p-10">
+          {priceLists.map((priceList, index) => (
+            <Accordion key={index} type="single" collapsible className="w-full">
+              <AccordionItem value="item-1">
+                <AccordionTrigger>{priceList.title}</AccordionTrigger>
+                <AccordionContent className="flex flex-col gap-5">
+                  {priceList.contents.map((content, index) => (
+                    <div key={index} className="flex flex-col items-center">
+                      {content.content?.map((content, index) => (
+                        <div
+                          key={index}
+                          className="w-full flex flex-row items-center gap-5 border-b-2 py-5  last:border-b-0"
+                        >
+                          {content.img ? (
+                            <Image
+                              src={content.img}
+                              quality={100}
+                              width={300}
+                              height={100}
+                              alt="img"
+                              className="rounded-xl w-1/5 h-28 object-cover"
+                            />
+                          ) : null}
+                          <div className="flex flex-col w-3/5">
+                            <div className="font-semibold text-base">
+                              {content.subtitle}
+                            </div>
+                            {content.descs ? (
+                              <div>
+                                {content.descs.map((desc, index) => (
+                                  <div key={index}>
+                                    {desc.desc?.map((item, index) => (
+                                      <div
+                                        key={index}
+                                        className="flex flex-col"
+                                      >
+                                        <p className="py-1">- {item.title}</p>
+                                        {item.subdescs ? (
+                                          <ul className="flex flex-col list-disc pl-10 gap-1">
+                                            {item.subdescs.map(
+                                              (subdesc, index) => (
+                                                <li key={index}>
+                                                  {subdesc.subdesc}
+                                                </li>
+                                              )
+                                            )}
+                                          </ul>
+                                        ) : null}
+                                      </div>
+                                    ))}
+                                  </div>
+                                ))}
+                              </div>
+                            ) : null}
+                          </div>
+
+                          {content.price ? (
+                            <div className="w-1/5">{content.price}</div>
+                          ) : null}
+                        </div>
+                      ))}
+                    </div>
+                  ))}
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          ))}
+        </Card>
+      </div>
     </div>
   );
 }
--- src/app/about_the_budget/to_download/page.tsx
+++ src/app/about_the_budget/to_download/page.tsx
@@ -1,4 +1,44 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
+import { FaRegFilePdf } from "react-icons/fa6";
+import { GrDownload } from "react-icons/gr";
+
+interface sectionItem {
+  item?: string;
+}
+interface ISection {
+  title: string;
+  sectionItems?: sectionItem[];
+}
+
+const downloadSection: ISection[] = [
+  {
+    title: "Legal regulations regarding the Edinburgh Citizens' Budget",
+    sectionItems: [
+      {
+        item: "Resolution on the principles of ECV as a special form of social consultation",
+      },
+      { item: "Ordinance on the appointment" },
+      {
+        item: "Ordinance on the principles and procedure for creating and implementing ECB 2025",
+      },
+      { item: "Ordinance amending" },
+    ],
+  },
+  {
+    title: "Support list for the Edinburgh Citizens' Budget project",
+    sectionItems: [{ item: "Support list.pdf" }],
+  },
+  {
+    title: "Map and division of ECB areas",
+    sectionItems: [{ item: "Area map.pdf" }, { item: "Boundary layout.pdf" }],
+  },
+  {
+    title: "Poster for Leaders - Voting Promotion",
+    sectionItems: [{ item: "ECB 2025 Poster.pdf" }],
+  },
+];
 
 function ToDownload() {
   return (
@@ -16,7 +56,34 @@ function ToDownload() {
           <p className="text-4xl font-bold text-white">BUDGET STEP BY STEP</p>
         </div>
       </div>
-      <div className="flex flex-row justify-center w-3/4 min-h-[65vh] "></div>
+      <div className="flex flex-row justify-center w-3/4 min-h-[65vh] ">
+        <Card className="p-10 flex flex-col gap-6">
+          {downloadSection.map((section, index) => (
+            <Card key={index}>
+              <CardHeader className="flex flex-col gap-5">
+                <CardTitle>{section.title}</CardTitle>
+                <CardContent className="flex flex-col gap-5">
+                  {section.sectionItems?.map((item, index) => (
+                    <div
+                      key={index}
+                      className="flex flex-row justify-between items-center bg-[#EEEEEE] hover:bg-[#DDDDDD] py-2 pl-1 pr-3 "
+                    >
+                      <div className="flex flex-row items-center gap-2 p-3">
+                        <FaRegFilePdf className="size-5" />
+                        {item.item}
+                      </div>
+                      <Button className="flex flex-row gap-2 items-center hover:cursor-pointer ">
+                        <GrDownload className="size-5" />
+                        <p>Download PDF file</p>
+                      </Button>
+                    </div>
+                  ))}
+                </CardContent>
+              </CardHeader>
+            </Card>
+          ))}
+        </Card>
+      </div>
     </div>
   );
 }
